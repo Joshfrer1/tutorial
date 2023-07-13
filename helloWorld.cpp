6e6320f3704e6fa79f547121b6cf067c46e820bc
marksoulier <122393887+marksoulier@users.noreply.github.com>
2023-07-13 00:55:46 -0600

    commit changes

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
+#include <cmath>
 using namespace std;
 using namespace std::chrono;
+
+double power(double num1, double num2) {
+    return pow(num1, num2);
+}
 
 double add(double num1, double num2) {
     return num1 + num2;
@@ -28,6 +33,11 @@ double getPercentage(double value, double sum) {
     return (value / sum) * 100.0;
 }
 
+int countPower(){
+    static int count = 0;
+    return count++;
+}
+
 int countAdd() {
     static int count = 0;
     return count++;
@@ -48,13 +58,14 @@ int countDivide() {
     return count++;
 }
 
-void getStats(double add, double subtract, double multiply, double divide) {
-    int sum = add + subtract + multiply + divide;
+void getStats(double add, double subtract, double multiply, double divide, double powers) {
+    int sum = add + subtract + multiply + divide + powers;
     printf("\n");
     cout << "Percent of times addition called: " << getPercentage(add, sum) << "%" << endl;
     cout << "Percent of times subtraction called: " << getPercentage(subtract, sum) << "%" << endl;
     cout << "Percent of times multiplication called: " << getPercentage(multiply, sum) << "%" << endl;
     cout << "Percent of times division called: " << getPercentage(divide, sum) << "%" << endl;
+    cout << "Percent of times power called: " << getPercentage(powers, sum) << "%" << endl;
     printf("\n");
 }
 
@@ -81,8 +92,13 @@ void compute(double num1, double num2, int operation) {
             cout << "Division: ";
             printResult(divide(num1, num2));
             break;
+        case 5:
+            countPower();
+            cout << "Power: ";
+            printResult(power(num1, num2));
+            break;
         default:
-            printf("\nOperation was not between 1 and 4: %d", operation);
+            printf("\nOperation was not between 1 and 5: %d", operation);
     }
 }
 
@@ -98,7 +114,7 @@ void simulate(double simulations) {
         // I tried to put them doubleo variables but realized quickly that since they were
         // stored in memory they would never randomize again so I would output the same
         // values as many times as I simulated.
-        compute(((rand() % (100 - 1 + 1)) + 1), ((rand() % (100 - 1 + 1)) + 1), ((rand() % (4 - 1 + 1)) + 1));
+        compute(((rand() % (10 - 1 + 1)) + 1), ((rand() % (10 - 1 + 1)) + 1), ((rand() % (5 - 1 + 1)) + 1));
         // the parameters are set such that a range can be defined for the randomizers.
         // Parameter 1 and 2 take in value randomly from 1 to 100.
         // Parameter 3 takes in a value for the switch case from 1 to 4.
@@ -110,7 +126,7 @@ void simulate(double simulations) {
     // get the difference and report
     auto duration = duration_cast<milliseconds>(end - start);
     cout << "\n" << "Execution time: " << duration.count() << " milliseconds" << endl;
-    getStats(countAdd(), countSubtract(), countMultiply(), countDivide());
+    getStats(countAdd(), countSubtract(), countMultiply(), countDivide(), countPower());
 }
 
 int main() {
